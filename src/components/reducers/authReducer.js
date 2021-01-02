cca4f73943ea5fcec7597a291702c3bbfe096588
AssanSav <assane.savadogo81@gmail.com>
2021-01-02 02:52:15 -0500

    refact

--- src/components/reducers/authReducer.js
+++ src/components/reducers/authReducer.js
@@ -2,8 +2,10 @@ import { FETCH_USER } from "../actions/types";
 
 const auth = (state = null, action) => {
   switch (action.type) {
-    case FETCH_USER:
-      return action.payload || false;
+    
+    case FETCH_USER: {
+      return typeof action.payload === "object" ? action.payload : false;
+    }
     default:
       return state;
   }
