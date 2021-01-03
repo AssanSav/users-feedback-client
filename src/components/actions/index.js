0e5a69c3266c5aaa41da29ae0466574b2d1e2738
AssanSav <assane.savadogo81@gmail.com>
2021-01-03 13:21:52 -0500

    Switched from axios to fetch method

--- src/components/actions/index.js
+++ src/components/actions/index.js
@@ -3,12 +3,21 @@ import { FETCH_USER } from "./types";
 
 let BASE_URL = process.env.REACT_APP_URL;
 
-export const fetchUser = () => async (dispatch) => {
-  const res = await axios.get(`${BASE_URL}/api/current_user`, {
-    withCredentials: true,
-  });
-  dispatch({ type: FETCH_USER, payload: res.data });
-};
+// export const fetchUser = () => async (dispatch) => {
+//   const res = await axios.get(`${BASE_URL}/api/current_user`, {
+//     withCredentials: true,
+//   });
+//   dispatch({ type: FETCH_USER, payload: res.data });
+// };
+
+export const fetchUser = () => dispatch => {
+  return fetch(`${BASE_URL}/api/current_user`, {
+    credentials: "include"
+  }).then(resp => resp.json())
+  .then((data) => {
+    dispatch({ type: FETCH_USER, payload: data })
+  })
+}
 
 export const handleToken = (token) => async (dispatch) => {
   const res = await axios.post(`${BASE_URL}/api/stripe`, token, {
