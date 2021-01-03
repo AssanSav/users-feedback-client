b28f5ba1204dc1264072d39df08de3ec60a8baf3
AssanSav <assane.savadogo81@gmail.com>
2021-01-03 14:43:32 -0500

    Refact

--- src/components/actions/index.js
+++ src/components/actions/index.js
@@ -3,21 +3,28 @@ import { FETCH_USER } from "./types";
 
 let BASE_URL = process.env.REACT_APP_URL;
 
-// export const fetchUser = () => async (dispatch) => {
-//   const res = await axios.get(`${BASE_URL}/api/current_user`, {
-//     withCredentials: true,
-//   });
-//   dispatch({ type: FETCH_USER, payload: res.data });
+// export const fetchUser = () => {
+//   return (dispatch) => {
+//     return fetch("https://users-feedback-app.herokuapp.com/", {
+//       headers: {
+//         "Conent-Type": "application/json",
+//         Accept: "application/json",
+//       },
+//       credentials: "include",
+//     })
+//       .then((resp) => resp.json())
+//       .then((data) => {
+//         dispatch({ type: FETCH_USER, payload: data });
+//       });
+//   };
 // };
 
-export const fetchUser = () => dispatch => {
-  return fetch(`${BASE_URL}/api/current_user`, {
-    credentials: "include"
-  }).then(resp => resp.json())
-  .then((data) => {
-    dispatch({ type: FETCH_USER, payload: data })
-  })
-}
+export const fetchUser = () => async (dispatch) => {
+  const res = await axios.get(`${BASE_URL}/api/current_user`, {
+    withCredentials: true,
+  });
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
 
 export const handleToken = (token) => async (dispatch) => {
   const res = await axios.post(`${BASE_URL}/api/stripe`, token, {
