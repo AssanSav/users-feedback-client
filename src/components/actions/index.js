d41abc7582910a2213a0064ef65328c5f863f810
AssanSav <assane.savadogo81@gmail.com>
2021-01-02 20:33:26 -0500

    Credentials included set to true

--- src/components/actions/index.js
+++ src/components/actions/index.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
 
+let BASE_URL = process.env.REACT_APP_URL;
+
 export const fetchUser = () => async (dispatch) => {
-  const res = await axios.get("https://users-feedback-app.herokuapp.com/api/current_user");
+  const res = await axios.get(`${BASE_URL}/api/users/current_user`, {
+    withCredentials: true,
+  });
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const handleToken = (token) => async (dispatch) => {
-  const res = await axios.post("/api/stripe", token);
+  const res = await axios.post(`${BASE_URL}/api/stripe`, token, {
+    withCredentials: true,
+  });
   dispatch({ type: FETCH_USER, payload: res.data });
 };
